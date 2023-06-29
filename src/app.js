6b845fe549db6528fd803008c0830c644f24fdbe
jocelinegarcia <42221352+jocelinegarcia@users.noreply.github.com>
2023-06-29 15:41:02 -0400

    Added script

--- src/app.js
+++ src/app.js
@@ -4,9 +4,9 @@ const app = express();
 const port = 8000;
 app.listen(port,()=> {
 console.log('listen port 8000');
-})
+});
 
 app.get('/hello_world', (req,res)=>{
     res.send('Hello World');
-    })
+    });
     
